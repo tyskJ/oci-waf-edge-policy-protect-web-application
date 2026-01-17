3ee82c18a6cb1082569a5ea160357c634aa5cc30
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-17 17:25:40 +0900

    :recycle: apache ssl 設定ファイル生成リソース追加

--- envs/aws/locals.tf
+++ envs/aws/locals.tf
@@ -12,3 +12,29 @@ locals {
     "ECDHE-ECDSA-AES128-GCM-SHA256"
   ])
 }
+
+/************************************************************
+Apache SSL Settings File
+************************************************************/
+resource "local_file" "apache_ssl_conf" {
+  filename = "${path.module}/../.settings/apache-ssl.conf"
+  content  = <<EOF
+<VirtualHost *:443>
+    ServerName origin.${var.domain_name}
+    DocumentRoot /var/www/html
+
+    SSLEngine on
+    SSLProtocol -all +TLSv1.2 +TLSv1.3
+    SSLCipherSuite ${local.apache_ssl_ciphers}
+    SSLHonorCipherOrder on
+
+    SSLCertificateFile /etc/pki/tls/certs/server.crt
+    SSLCertificateKeyFile /etc/pki/tls/private/server.key
+    SSLCACertificateFile /etc/pki/tls/certs/ca.crt
+
+    <Directory "/var/www/html">
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+}

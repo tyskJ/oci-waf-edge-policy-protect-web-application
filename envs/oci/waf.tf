0c1f900b481689c6e349c404b372192b0d416ca5
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-18 00:54:01 +0900

    :recycle: WAFサーバー用証明書登録リソース修正

--- envs/oci/waf.tf
+++ envs/oci/waf.tf
@@ -3,9 +3,9 @@ Certificates for Edge Policy
 ************************************************************/
 resource "oci_waas_certificate" "this" {
   compartment_id                 = oci_identity_compartment.workload.id
-  display_name                   = "origin-cert"
-  certificate_data               = file("${path.module}/../.key/server_crt.pem")
-  private_key_data               = file("${path.module}/../.key/server_key.pem")
+  display_name                   = "edge-policy-cert"
+  certificate_data               = tls_locally_signed_cert.server.cert_pem
+  private_key_data               = tls_private_key.server.private_key_pem
   is_trust_verification_disabled = true # Self-signed Certificates
   defined_tags                   = local.common_defined_tags
 }

6bca145a7dffdd2a474e3332aae75f292f0d5dea
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-17 16:00:15 +0900

    :memo: ec2再起動時に、再実行するように設定 シェルスクリプトと、cloud-configを同時に使うため、MIMEマルチパート形式とする

--- envs/aws/userdata/linux_init.sh
+++ envs/aws/userdata/linux_init.sh
@@ -1,3 +1,22 @@
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
 #!/bin/bash
 
 # Shell Options
@@ -27,3 +46,4 @@ cat /etc/os-release >> /var/www/html/index.html
 echo '</code></pre></body></html>' >> /var/www/html/index.html
 chown -R apache:apache /var/www/html
 systemctl enable --now httpd
+--//--

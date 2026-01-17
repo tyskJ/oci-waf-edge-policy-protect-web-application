466c8ada50cbf37365c2de9e17095b8d00f425fb
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-17 14:55:01 +0900

    :recycle: amazon linux 2023取得定義

--- envs/aws/data.tf
+++ envs/aws/data.tf
@@ -3,3 +3,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 data "aws_partition" "current" {}
+
+data "aws_ssm_parameter" "amazonlinux_2023" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64"
+}

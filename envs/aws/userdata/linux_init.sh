650911b63a7587cc0eebeb6e50475b7bbe063a4f
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-17 17:29:04 +0900

    :recycle: apache ssl conf 設定追加

--- envs/aws/userdata/linux_init.sh
+++ envs/aws/userdata/linux_init.sh
@@ -37,12 +37,38 @@ localectl set-locale LANG=ja_JP.UTF-8
 localectl set-keymap jp-OADG109A
 
 # Apach Install
-dnf install -y httpd
+dnf install -y httpd mod_ssl
+
+# Apache Document Root settings
 echo '<html><head></head><body><pre><code>' > /var/www/html/index.html
 hostname >> /var/www/html/index.html
 echo '' >> /var/www/html/index.html
 cat /etc/os-release >> /var/www/html/index.html
 echo '</code></pre></body></html>' >> /var/www/html/index.html
 chown -R apache:apache /var/www/html
+
+# Apache SSL Settings
+mkdir -p /etc/pki/tls/private
+mkdir -p /etc/pki/tls/certs
+
+cat <<'EOF' > /etc/pki/tls/private/server.key
+${server_key}
+EOF
+
+cat <<'EOF' > /etc/pki/tls/certs/server.crt
+${server_cert}
+EOF
+
+cat <<'EOF' > /etc/pki/tls/certs/ca.crt
+${ca_cert}
+EOF
+
+chmod 600 /etc/pki/tls/private/server.key
+
+cat <<'EOF' > /etc/httpd/conf.d/ssl.conf
+${ssl_conf}
+EOF
+
+# Apache Start
 systemctl enable --now httpd
 --//--

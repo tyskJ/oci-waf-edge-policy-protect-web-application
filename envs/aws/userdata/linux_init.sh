52433ab692972faec4e55696e7ebd4ebff3ac747
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-17 15:16:20 +0900

    :recycle: apacheインストールスクリプト追加

---
+++ envs/aws/userdata/linux_init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Shell Options
+# e : エラーがあったら直ちにシェルを終了
+# u : 未定義変数を使用したときにエラーとする
+# o : シェルオプションを有効にする
+# pipefail : パイプラインの返り値を最後のエラー終了値にする (エラー終了値がない場合は0を返す)
+set -euo pipefail
+
+# Package Update
+dnf update -y
+
+# Apach Install
+dnf install -y httpd
+echo '<html><head></head><body><pre><code>' > /var/www/html/index.html
+hostname >> /var/www/html/index.html
+echo '' >> /var/www/html/index.html
+cat /etc/os-release >> /var/www/html/index.html
+echo '</code></pre></body></html>' >> /var/www/html/index.html
+chown -R apache:apache /var/www/html
+systemctl enable --now httpd
